7b22a954c4337ccd958df9bf617539226e9cff58
CesarQuint <pedrocesarquintero001@gmail.com>
2023-03-10 19:08:04 -0600

    Creating Project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./Layout/Header"
+import Footer from "./Layout/Footer"
+
+function App() {
+
+  return (
+   <>
+    <Header/>
+      <main>
+        
+      </main>
+    <Footer/>
+   </>
+  )
+}
+
+export default App
---
+++ src/Layout/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+        <h1>My Pet Care</h1>
+    </header>
+  )
+}
+
+export default Header
